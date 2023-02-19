da5c1e2def4e4c017c18ee72e5dc22a669d0d4f2
parag <paragjain528@gmail.com>
2023-02-19 12:55:28 +0530

    Nested Route in welcome page

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,9 +1,13 @@
+import { Route } from "react-router-dom";
 const Welcome =()=>{
 
 
     return (
         <h1>
        Welcome page
+       <Route path="/welcome/new-user">
+       <p>welcome new user</p>
+       </Route>
         </h1>
     )
 }
